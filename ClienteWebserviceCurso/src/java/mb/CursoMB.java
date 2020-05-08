c12d483b524992d31056d91fc84594adc50b7831
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-05-08 02:36:38 -0300

    Adiciona projeto client

---
+++ ClienteWebserviceCurso/src/java/mb/CursoMB.java
@@ -0,0 +1,75 @@
+package mb;
+
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.enterprise.context.SessionScoped;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import modelo.Curso;
+
+@ManagedBean
+@SessionScoped
+public class CursoMB implements Serializable{
+    private Curso curso = new Curso();
+    private List<Curso> cursos = new ArrayList();
+    private final Client client = ClientBuilder.newClient();
+    private final Gson gson = new Gson();
+    
+    public CursoMB(){
+        recarregar();
+    }
+    
+    private void recarregar(){
+        WebTarget webtarget = client.target("http://localhost:8080/CursosWebserviceRest/recursos/curso");
+        Invocation.Builder invocationBuilder = webtarget.request(MediaType.APPLICATION_JSON + "; charset=utf-8");
+        String json = invocationBuilder.get(String.class);
+        
+        cursos = gson.fromJson(json, new TypeToken<List<Curso>>(){}.getType());
+    }
+    
+    public String salvar() {
+        WebTarget webtarget = client.target("http://localhost:8080/CursosWebserviceRest/recursos/curso");
+        Invocation.Builder invocationBuilder = webtarget.request(MediaType.APPLICATION_JSON + "; charset=utf-8");
+        Response response = invocationBuilder.post(Entity.entity(curso, MediaType.APPLICATION_JSON + "; charset=utf-8"));
+        
+        curso = new Curso();
+        recarregar();
+        return "index";
+    }
+    
+    public void editar(Curso c){
+        this.curso = c;
+    }
+    
+    public void remover(Curso c){
+        WebTarget webtarget = client.target("http://localhost:8080/CursosWebserviceRest/recursos/curso/" + c.getId());
+        Invocation.Builder invocationBuilder = webtarget.request(MediaType.APPLICATION_JSON + "; charset=utf-8");
+        Response response = invocationBuilder.delete();
+        recarregar();
+    }
+    
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public List<Curso> getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(List<Curso> cursos) {
+        this.cursos = cursos;
+    }
+}
---
+++ ClienteWebserviceCurso/src/java/modelo/Curso.java
@@ -0,0 +1,67 @@
+package modelo;
+
+public class Curso {
+    private int id;
+    private String nome;
+    private String descricao;
+    private float preco;
+    private String codigo;
+    private int duracao;
+    private int avaliacao;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public int getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(int avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+}
